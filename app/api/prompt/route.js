6ceb5d99758c2de13e57703beb55154675e4f20d
Duy Le <dle2@oberlin.edu>
2023-07-07 11:36:24 +0700

    4th - disable caching

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,16 +2,11 @@ import { connectedToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (req) => {
-  try {
+    try {
     await connectedToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    const response = new Response(JSON.stringify(prompts), { status: 200 });
-    response.headers.set("Cache-Control", "no-store");
-    return response;
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    return new Response(
-      JSON.stringify(`Failed to fetch all prompts, error: ${error}`),
-      { status: 500 }
-    );
+    return new Response(JSON.stringify(`fail to fetch all prompts, error: ${error}`), { status: 500 });
   }
-};
+}
