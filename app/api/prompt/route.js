06833e36deb0c8985f78f6262b79898826375b34
Duy Le <dle2@oberlin.edu>
2023-07-07 10:56:44 +0700

    Third commit

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,11 +2,16 @@ import { connectedToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (req) => {
-    try {
+  try {
     await connectedToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    const response = new Response(JSON.stringify(prompts), { status: 200 });
+    response.headers.set("Cache-Control", "no-store");
+    return response;
   } catch (error) {
-    return new Response(JSON.stringify(`fail to fetch all prompts, error: ${error}`), { status: 500 });
+    return new Response(
+      JSON.stringify(`Failed to fetch all prompts, error: ${error}`),
+      { status: 500 }
+    );
   }
-}
+};
